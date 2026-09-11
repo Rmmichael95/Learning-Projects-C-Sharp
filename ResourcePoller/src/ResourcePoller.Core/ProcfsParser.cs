eee441999a83c481f34ea23c4609abfeebe0eaa5
Ryan M Sullivan <rmmichael95@gmail.com>
2026-09-11 14:50:09 -0700

    Passing test

--- ResourcePoller/src/ResourcePoller.Core/ProcfsParser.cs
+++ ResourcePoller/src/ResourcePoller.Core/ProcfsParser.cs
@@ -9,15 +9,37 @@ namespace ResourcePoller.Core
     {
         public static SystemMetrics ParseMemInfo(string memInfoContent)
         {
-            var metrics = new SystemMetrics();
+            SystemMetrics metrics = new();
 
             // TODO: 1. Split the incoming massive string by newline characters.
             // TODO: 2. Loop through the lines.
             // TODO: 3. If line starts with "MemTotal:", extract the number, parse to long, assign to metrics.TotalMemoryKb.
             // TODO: 4. Repeat for "MemFree:" and "MemAvailable:".
             // TODO: 5. Return the populated metrics object.
+            string[] lines = memInfoContent.Split('\n');
 
-            throw new NotImplementedException();
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("MemTotal:"))
+                {
+                    metrics.TotalMemoryKb = Convert.ToInt64(
+                        line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]
+                    );
+                }
+                else if (line.StartsWith("MemFree:"))
+                {
+                    metrics.FreeMemoryKb = Convert.ToInt64(
+                        line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]
+                    );
+                }
+                else if (line.StartsWith("MemAvailable:"))
+                {
+                    metrics.AvailableMemoryKb = Convert.ToInt64(
+                        line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]
+                    );
+                }
+            }
+            return metrics;
         }
     }
 }
